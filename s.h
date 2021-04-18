aa0d96a381da4f6a5ba6b306145e7b3a8f3c7930
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2021-04-18 05:21:09 +0100

    Tentative cosmopolitan libc support

--- s.h
+++ s.h
@@ -1,7 +1,8 @@
-/* short.hand */
+/* (cosmo)short.hand */
 #ifndef SHORTHAND_H_INCLUDED
 #define SHORTHAND_H_INCLUDED
 
+#ifndef COSMOPOLITAN_H_ /* proxy for "noncosmipolitan setup" */
 #include <assert.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -10,66 +11,51 @@
 #include <string.h>
 #include <math.h>
 #include <time.h>
+#else
+/* trialing Cosmopolitan libc support, cosmopolitan.h has all the standard ones I use */
+#include "cosmo/cosmopolitan.h" /* we assumed cosmipolitan above, this is just for linting etc. */
+#endif
 
 typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
-typedef uint8_t u8;
+typedef uint8_t  u8;
 
 #define u64_MAX UINT64_MAX
 #define u32_MAX UINT32_MAX
 #define u16_MAX UINT16_MAX
-#define u8_MAX UINT8_MAX
+#define u8_MAX  UINT8_MAX
 
 typedef int16_t i16;
 typedef int32_t i32;
 typedef int64_t i64;
-typedef int8_t i8;
+typedef int8_t  i8;
 
 #define i64_MAX INT64_MAX
 #define i32_MAX INT32_MAX
 #define i16_MAX INT16_MAX
-#define i8_MAX INT8_MAX
+#define i8_MAX  INT8_MAX
 #define i64_MIN INT64_MIN
 #define i32_MIN INT32_MIN
 #define i16_MIN INT16_MIN
-#define i8_MIN INT8_MIN
+#define i8_MIN  INT8_MIN
 
 typedef int16_t s16;
 typedef int32_t s32;
 typedef int64_t s64;
-typedef int8_t s8;
+typedef int8_t  s8;
 
 #define s64_MAX INT64_MAX
 #define s32_MAX INT32_MAX
 #define s16_MAX INT16_MAX
-#define s8_MAX INT8_MAX
+#define s8_MAX  INT8_MAX
 #define s64_MIN INT64_MIN
 #define s32_MIN INT32_MIN
 #define s16_MIN INT16_MIN
-#define s8_MIN INT8_MIN
+#define s8_MIN  INT8_MIN
 
 typedef double f64;
 typedef float f32;
-
-#ifdef _WIN32
-/* use in a "... %"LU" ..." pattern, allows formatting */
-#define LU "llu"
-#define LD "lld"
-#define LN "\r\n"
-#else
-/* use in a "... %"LU" ..." pattern, allows formatting */
-#define LU "lu"
-#define LD "ld"
-#define LN "\n"
-#endif
-
-#ifndef printvar
-#define printvar(var) printf("%s=%d"LN,(#var),(var))
-#endif
-
-/* just prints a newline */
-#define println() printf(LN)
 
 #if !defined(bool) && defined(_Bool)
 #define bool _Bool
@@ -79,6 +65,7 @@ typedef float f32;
 #define bool char
 #endif
 
+/* personal preference */
 #define not !
 #define and &&
 #define or ||
@@ -133,10 +120,6 @@ typedef float f32;
 #define is_pow2(n) ((n) && !((n) & ((n)-1)))
 #endif
 
-#ifndef PI
-#define PI 3.141592653589793238
-#endif
-
 #ifndef which_bit
 /* given a value with only a single bit set, which bit is it? */
 /* (https://graphics.stanford.edu/~seander/bithacks.html#IntegerLog) */
@@ -159,24 +142,24 @@ u32 which_bit(u32 v) {
 #define bits(n) (((n) + 7) / 8)
 
 /* x's value in the n'th bit */
-#define bs_in(x, n) ((x)[(n)/8] & (1 << ((n) % 8)))
+#define bs_in(x, n) ((x)[(n) / 8] & (1 << ((n) % 8)))
 
 /* set x's n'th bit to 1 */
-#define bs_set(x, n) ((x)[(n)/8] |= (1 << ((n) % 8)))
+#define bs_set(x, n) ((x)[(n) / 8] |= (1 << ((n) % 8)))
 
 /* set x's n'th bit to 0 */
-#define bs_clear(x, n) ((x)[(n)/8] &= ~(1 << ((n) % 8)))
+#define bs_clear(x, n) ((x)[(n) / 8] &= ~(1 << ((n) % 8)))
 
 /* set x's bits from n on to 0 */
-#define bs_clearall(x, n) ((x)[(n)/8] &= ~((1 << (((n) % 8) + 1))-1))
+#define bs_clearall(x, n) ((x)[(n) / 8] &= ~((1 << (((n) % 8) + 1))-1))
 
 /* toggle x's n'th bit */
-#define bs_toggle(x, n) ((x)[(n)/8] ^= (1 << ((n) % 8)))
+#define bs_toggle(x, n) ((x)[(n) / 8] ^= (1 << ((n) % 8)))
 
-#if !defined(bitcount64)
-i32 bitcount64(uint64_t v) { /* clang-10 favoured bc. 64 bit magic??? */
+#if !defined(bitcount)
+i32 bitcount64(u64 v) { /* clang-10 favoured bc. 64 bit magic??? */
   i32 r = 0;
-  while(v != 0) {
+  while (v != 0) {
     v &= v - 1;
     r++;
   }
@@ -206,6 +189,11 @@ bool prime(u32 n) {
   return true;
 }
 #endif
+
+/* just prints a newline */
+void println() {
+  printf("%n");
+}
 
 /* 2 u32s into a u64, `a` goes into "left" (higher) bits */
 #define packu64(a, b) (((u64)(a)) << 32 | (b))
