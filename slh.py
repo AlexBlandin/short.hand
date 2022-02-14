a81c5c52013339ff11853672956520841f488ea9
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-02-14 22:24:40 +0000

    Faster readlines(fp) and isqrt(n) if n<2**52
    readlinesmap is kinda just there to be handy, save having to keep wrapping in str.lower or str.upper or things like len, particularly when sequentially.
    
    Also I'm not fussed about line count anymore, so might as well get IDE popups for each function so I don't need to check the source when I forget a detail.

--- slh.py
+++ slh.py
@@ -1,36 +1,47 @@
 # Santa's Little Helpers
+from operator import indexOf, itemgetter
 from functools import partial, reduce # just so they're on hand
 from random import sample, randrange
 from dataclasses import dataclass
-from operator import itemgetter
 from datetime import datetime
+from math import prod, sqrt # good to have on hand (esp. now pypy supports 3.8)
 from itertools import chain
 from pathlib import Path
 from time import time
-from math import prod # to have on hand (now pypy supports 3.8)
-import sys
+import itertools as it # for it.count() etc
+import sys, os
 
-class dot(dict): # as in a "dot dict", a dict you can access by a "." # pretty inefficient bc. (dict), but convenient
+try: # another nice to have, currently locked behind 3.9 for "best" memoizing (faster than lru_cache)
+  from functools import cache
+except:
+  from functools import lru_cache as cache
+
+class dot(dict):
+  """as in a "dot dict", a dict you can access by a "." # pretty inefficient bc. (dict), but convenient"""
   __getattr__, __setattr__ = dict.__getitem__, dict.__setitem__
 
 def slots(self):
   return {slot:self.__getattribute__(slot) for slot in self.__slots__}
 
 @dataclass
-class data: # I recommend this pattern in general for POD, slightly more memory with __slots__ but guarantees perf
+class data:
+  "I recommend this pattern in general for POD, slightly more memory with __slots__ but guarantees perf"
   __slots__ = ["x", "y", "z", "w"]
   x: float
   y: float
   z: float
   w: float
-  def slots(self): # rather helpful for introspection or debugging
+  def slots(self):
+    "rather helpful for introspection or debugging"
     return {slot:self.__getattribute__(slot) for slot in self.__slots__}
-  def __dict__(self): # direct method isn't required, but faster, vars(d)() == d.slots()
+  def __dict__(self):
+    "direct method isn't required, but faster, vars(d)() == d.slots()"
     return {slot:self.__getattribute__(slot) for slot in self.__slots__}
 
 try: # 3.10+
   @dataclass(slots=True)
-  class quickdata: # Nicer, only some linters realise __slots__ get autogen'd
+  class quickdata:
+    "Nicer, only some linters realise __slots__ get autogen'd"
     x: float
     y: float
     z: float
@@ -38,25 +49,33 @@ try: # 3.10+
 except:
   pass
 
-def sorted_dict_by_key(d: dict, reverse=False): # sort a dict by key
+def sorted_dict_by_key(d: dict, reverse=False):
+  "sort a dict by key"
   return dict(sorted(d.items(), key=itemgetter(0), reverse=reverse))
 
-def sorted_dict_by_val(d: dict, reverse=False): # sort a dict by value
+def sorted_dict_by_val(d: dict, reverse=False):
+  "sort a dict by value"
   return dict(sorted(d.items(), key=itemgetter(1), reverse=reverse))
 
-def sorted_dict(d, key=itemgetter(1), reverse=True): return dict(sorted(d.items(), key=key, reverse=reverse))
+def sorted_dict(d, key=itemgetter(1), reverse=False):
+  "generic sorting, because it's something people kinda want"
+  return dict(sorted(d.items(), key=key, reverse=reverse))
 
-def sortas(first: list, second: list): # sorts the first as if it was the second
+def sortas(first: list, second: list):
+  "sorts the first as if it was the second"
   return list(map(itemgetter(0), sorted(zip(first,second), key=itemgetter(1))))
 
-def bits(x: int): # because bin() has the annoying 0b, so slower but cleaner
+def bits(x: int):
+  "because bin() has the annoying 0b, so slower but cleaner"
   return f"{x:b}"
 
-def ilog2(x): # integer log2, aka the position of the first bit
+def ilog2(x):
+  "integer log2, aka the position of the first bit"
   return x.bit_length()-1
 
-# from gmpy2 import bit_scan1 as ctz
-def ctz(v): # count trailing zeroes
+# from gmpy2 import bit_scan1 as ctz # if you must go faster
+def ctz(v):
+  "count trailing zeroes"
   return (v & -v).bit_length()-1
 
 if sys.version_info.major >= 3 and sys.version_info.minor >= 10:
@@ -75,13 +94,17 @@ def from_bytes(b: bytes, signed=False, byteorder=sys.byteorder) -> int:
   "int.from_bytes but sensible byteorder, you must say if it's signed"
   return int.from_bytes(b, byteorder, signed)
 
-def isqrt(n: int): # not as fast as int(sqrt) but works for all ints, not just those in f64 integer range
+def isqrt(n: int):
+  "works for all ints, fast for numbers < 2**52 (aka, abusing double precision sqrt)"
+  if n < 2**52:
+    return int(sqrt(n))
   x, y = n, (n + 1)//2
   while y < x:
     x, y = y, (y + n//y)//2
   return x
 
-def isprime(n: int): # simple iterative one
+def isprime(n: int):
+  "simple iterative one"
   if n in {2, 3, 5, 7}:
     return True
   if not (n & 1) or not (n % 3) or not (n % 5) or not (n % 7):
@@ -140,54 +163,70 @@ def fastprime(n: int, trials=8):
 
 flatten = chain.from_iterable
 
-def lmap(f, *args): # because wrapping in list() all the time is awkward, saves abusing the slow `*a,=map(*args)`!
+def lmap(f, *args):
+  "because wrapping in list() all the time is awkward, saves abusing the slow `*a,=map(*args)`!"
   return list(map(f, *args))
 
-def transpose(matrix): # not the fastest way, so if it's a big matrix please don't use
+def transpose(matrix):
+  "inefficient but elegant, so if it's a big matrix please don't use"
   return lmap(list, zip(*matrix))
 
-def tmap(f, *args): # for the versions of python with faster tuple lookups (PEP 590 vectorcalls effect this how?)
+def tmap(f, *args):
+  "for the versions of python with faster tuple lookups (TODO: PEP 590 vectorcalls effect this how?)"
   return tuple(map(f, *args))
 
-def join(iterable, sep = " "): # because sep.join(it) doesn't convert str(it) for it in iterable
+def join(iterable, sep = " "):
+  "because sep.join(it) doesn't convert str(it) for it in iterable"
   return sep.join(map(str, iterable))
 
-def avg(iterable, start=0): # because x/0 = 0 in euclidean
+def avg(iterable, start=0):
+  "without exceptions, because x/0 = 0 in euclidean"
   return sum(iterable, start) / len(iterable) if len(iterable) else 0
 
-def minmax(*iterable): # get the minimum and maximum quickly
-  return min(iterable),max(iterable) # min(*iterable) is only faster for len(iterable) == 2
+def minmax(*iterable):
+  "get the minimum and maximum quickly"
+  return min(iterable), max(iterable) # min(*iterable) is only faster for len(iterable) == 2
 
-def minmax_ind(*iterable): # minmax but with indices, so ((i_a,min),(i_b,max))
-  return min(enumerate(iterable),key=itemgetter(1)),max(enumerate(iterable),key=itemgetter(1))
+def minmax_ind(*iterable):
+  "minmax but with indices, so ((i_a,min),(i_b,max))"
+  return min(enumerate(iterable),key=itemgetter(1)), max(enumerate(iterable),key=itemgetter(1))
 
-def shuffled(*iterable): # aka, "shuffle but not in place", ie. reversed and sorted, but they ignored prior
+def shuffled(*iterable):
+  """aka, "shuffle but not in place", ie. reversed and sorted, but they ignored prior"""
   iterable = list(iterable) # this way we support sets
   return sample(iterable, len(iterable))
 
-def lenfilter(iterable, pred=bool): # counts how many are true for a given predicate
+def lenfilter(iterable, pred=bool):
+  "counts how many are true for a given predicate"
   return sum(1 for it in iterable if pred(it)) # better (esp in pypy) than len(filter()) since not constructing a list
 
-def first(iterable, default=None): # first item
+def first(iterable, default=None):
+  "the first item in iterable"
   return next(iter(iterable), default) 
 
 def dotprod(A, B):
   return sum(a*b for a,b in zip(A,B))
 
 def sample_set(s: set, k: int): # 3.9 was doing so many things right, this is just annoying
+  "sample a set because you just want some random elements and don't care"
   return sample(list(s), k) # if you really care about reproducibility (with known seeds) then sure, use sorted()
 
 def now():
   return f"{datetime.now():%Y-%m-%d-%H-%M-%S}"
 
-def tf(func, *args, **kwargs): # time func
+def tf(func, *args, print_result=True, **kwargs):
+  "time func func, as in, time the function func"
   start = time()
   r = func(*args, **kwargs)
   end = time()
-  print(f"{func.__qualname__}({', '.join(list(map(str,args)) + [f'{k}={v}' for k,v in kwargs.items()])}) = {r}, took {human_time(end-start)}")
+  if print_result:
+    print(f"{func.__qualname__}({', '.join(list(map(str,args)) + [f'{k}={v}' for k,v in kwargs.items()])}) = {r}, took {human_time(end-start)}")
+  else:
+    print(f"{func.__qualname__}({', '.join(list(map(str,args)) + [f'{k}={v}' for k,v in kwargs.items()])}), took {human_time(end-start)}")
   return r
 
-def human_time(t: float, seconds = True): # because nobody makes it humanly readable
+def human_time(t: float, seconds = True):
+  "because nobody makes it humanly readable"
   return f"{int(t//60)}m {human_time((int(t)%60)+(t-int(t)), True)}" if t > 60 else \
          f"{t:.3f}s" if t > 0.1 and seconds else                                    \
          f"{t*1000:.3f}ms" if t > 0.0001 else                                       \
@@ -209,18 +248,37 @@ def hours_minutes_seconds(t: float):
         print(f"{weeks}w{days}d{hours}h{minutes}m{seconds}s")
   print()
 
-def find(v, iterable: list, start = 0, stop = -1, missing = -1): # find v in interable without exceptions
+def find(v, iterable: list, start = 0, stop = -1, missing = -1):
+  "find the first index of v in interable without raising exceptions"
   try:
     return iterable.index(v, start, stop)
   except: # because if doesn't have .index then we couldn't find iterable
+    if start == 0 and stop == -1: # unless we aren't messing with start and stop, we might have a chance
+      try:
+        indexOf(iterable, v)
+      except:
+        pass
     return missing
 
 def yesno(msg="", accept_return=True, replace_lists=False, yes_list=set(), no_list=set()):
+  "Keep asking until they say yes or no"
   while True:
     reply = input(f"{msg} [y/N]: ").strip().lower()
     if reply in (yes_list if replace_lists else {"y", "ye", "yes"} | yes_list) or (accept_return and reply == ""): return True
     if reply in (no_list if replace_lists else {"n", "no"} | no_list): return False
 
-def readlines(self: Path, hint=-1, encoding="utf8"): # just reads a line
-  with self.open(encoding=encoding) as f:
-    return f.readlines(hint)
+# these readlines methods return lists because I often want it all in memory to use in a set or such, and these are convenience functions
+def readlines(fp: str | Path, encoding = "utf8"):
+  """just reads lines as you normally would want to, why wouldn't you strip "\\n" from each line?"""
+  return lmap(str.rstrip, Path(fp).read_text(encoding).splitlines())
+
+def readlinesmap(fp: str | Path, *fs, encoding = "utf8"):
+  "readlines but map functions in fs (by order of index, fs[0] first)"
+  def rec(fs: list):
+    if len(fs):
+      f = fs.pop()
+      return map(f, rec(fs))
+    else:
+      return map(str.rstrip, Path(fp).read_text(encoding).splitlines())
+  return list(rec(list(fs)))
+
