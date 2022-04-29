9e1bacc9c37e10e20fdef1b95a6765eeaff1273c
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-04-29 18:07:37 +0100

    Update pip-common.cmd

--- pip-common.cmd
+++ pip-common.cmd
@@ -1,16 +1,13 @@
 #!/usr/bin/env sh
 pip install -U pip
-pip install -U setuptools wheel
-pip install -U xxhash PyNaCl pyOpenSSL cryptography certifi
-pip install -U cffi pytest pdbpp rope rtoml tomli tomli-w psutil py-ulid disjoint-set
+pip install -U setuptools wheel virtualenv
+pip install -U PyNaCl pyOpenSSL cryptography certifi
+pip install -U mypy yapf pylint cffi pytest pdbpp psutil
 pip install -U numpy pandas scipy
 : # pip install -U tensorflow scikit-image
-pip install -U attrs fastcore arrow pendulum parse pylev requests websockets aiohttp async-timeout tqdm
-pip install -U colorama termcolor rich tldr humanize urllib3 httpie rethinkdb yapf pylint
-pip install -U Pillow matplotlib mpl_plotter
-: # pip install -U imgui[full]
-: # pip install -U dearpygui
-pip install -U present beautifulsoup4 soupsieve pypandoc
-pip install -U virtualenv yt-dlp
-pip install -U geocoder skyfield
+pip install -U attrs fastcore arrow pendulum parse pylev rope rtoml tomli tomli-w humanize
+pip install -U tqdm colorama termcolor rich tldr rethinkdb py-ulid present pypandoc
+pip install -U requests websockets aiohttp async-timeout urllib3 httpie beautifulsoup4 soupsieve
+pip install -U yt-dlp geocoder skyfield
+pip install -U Pillow matplotlib mpl_plotter dearpygui
 : # pip install -U pywin32 pyreadline
