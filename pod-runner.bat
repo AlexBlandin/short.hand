cc1d822a7cabce0c08110b9150091a17c69e9d5a
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2024-04-07 14:53:02 +0100

    Reset to use new format

--- pod-runner.bat
+++ pod-runner.bat
@@ -5,19 +5,19 @@ echo Hello, and welcome, to this batch of POD testing pods.
 
 :start
 echo Running through pods now.
-call python pod.py
+call python src\pod.py
 call wait 30
-call pypy pod.py
+call pypy src\pod.py
 call wait 30
-call python312 pod.py
+call python312 src\pod.py
 call wait 30
-call python311 pod.py
+call python311 src\pod.py
 call wait 30
-call python310 pod.py
+call python310 src\pod.py
 call wait 30
-call python39 pod.py
+call python39 src\pod.py
 call wait 30
-call python38 pod.py
+call python38 src\pod.py
 call wait 30
 
 echo Press <CTRL> + <C> to stop, or press <ENTER> to do again
